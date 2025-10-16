6bdc619b3e66e9c1d1f101bef9ce6325a170f0c0
fedelaurna <142011660+fedelaurna@users.noreply.github.com>
2025-10-16 11:03:52 -0300

    Add files via upload

---
+++ nba.js
@@ -0,0 +1,39 @@
+let equiposNBA = [];
+
+// Cargar equipos desde la API
+fetch("https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=NBA")
+  .then(res => res.json())
+  .then(data => {
+    equiposNBA = data.teams;
+    const select = document.getElementById("select-equipo");
+
+    equiposNBA.forEach(team => {
+      const option = document.createElement("option");
+      option.value = team.idTeam;
+      option.textContent = team.strTeam;
+      select.appendChild(option);
+    });
+  });
+
+// Mostrar info del equipo seleccionado
+document.getElementById("select-equipo").addEventListener("change", function() {
+  const idSeleccionado = this.value;
+  const equipo = equiposNBA.find(t => t.idTeam === idSeleccionado);
+  const infoDiv = document.getElementById("equipo-info");
+
+  if (equipo) {
+    const imgSrc = equipo.strTeamBadge || "https://via.placeholder.com/120x120?text=No+Image";
+
+    infoDiv.innerHTML = `
+      <img src="${imgSrc}" alt="Escudo de ${equipo.strTeam}" />
+      <h2>${equipo.strTeam}</h2>
+      <p><strong>Ciudad / Localización:</strong> ${equipo.strStadiumLocation || 'Desconocida'}</p>
+      <p><strong>Año de creación:</strong> ${equipo.intFormedYear || 'Desconocido'}</p>
+      <p><strong>Estadio:</strong> ${equipo.strStadium || 'Desconocido'}</p>
+      <p class="description"><em>Descripción:</em><br>${equipo.strDescriptionEN || 'Sin descripción disponible.'}</p>
+
+    `;
+  } else {
+    infoDiv.innerHTML = "";
+  }
+});
