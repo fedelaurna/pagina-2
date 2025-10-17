128fdb253b807b5c205b3218180b7f21feb7cce1
fedelaurna <142011660+fedelaurna@users.noreply.github.com>
2025-10-16 23:38:59 -0300

    Update nba.js

--- nba.js
+++ nba.js
@@ -3,25 +3,25 @@ let equiposNBA = [];
 fetch("https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=NBA")
   .then(res => res.json())
   .then(data => {
-    equiposNBA = data.teams;
-    const select = document.getElementById("select-equipo");
+    equiposNBA = data.teams
+    const select = document.getElementById("select-equipo")
 
     equiposNBA.forEach(team => {
-      const option = document.createElement("option");
-      option.value = team.idTeam;
-      option.textContent = team.strTeam;
-      select.appendChild(option);
-    });
-  });
+      const option = document.createElement("option")
+      option.value = team.idTeam
+      option.textContent = team.strTeam
+      select.appendChild(option)
+    })
+  })
 
 
 document.getElementById("select-equipo").addEventListener("change", function() {
-  const idSeleccionado = this.value;
-  const equipo = equiposNBA.find(t => t.idTeam === idSeleccionado);
-  const infoDiv = document.getElementById("equipo-info");
+  const idSeleccionado = this.value
+  const equipo = equiposNBA.find(t => t.idTeam == idSeleccionado)
+  const infoDiv = document.getElementById("equipo-info")
 
   if (equipo) {
-    const imgSrc = equipo.strTeamBadge || "https://via.placeholder.com/120x120?text=No+Image";
+    const imgSrc = equipo.strTeamBadge || "https://via.placeholder.com/120x120?text=No+Image"
 
     infoDiv.innerHTML = `
       <img src="${imgSrc}" alt="Escudo de ${equipo.strTeam}" />
@@ -31,7 +31,7 @@ document.getElementById("select-equipo").addEventListener("change", function() {
       <p><strong>Estadio:</strong> ${equipo.strStadium || 'Desconocido'}</p>
       <p class="description"><em>Descripción:</em><br>${equipo.strDescriptionEN || 'Sin descripción disponible.'}</p>
 
-    `;
+    `
   
   }
-});
+})
